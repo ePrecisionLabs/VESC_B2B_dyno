81f71f3b38348ab46e0ac9738e007211e8f52b92
Simon Roy <simon.roy.16@gmail.com>
2024-10-30 08:24:29 -0400

    Add number of loop cycles to profile script

--- TorqueSpeedProfile.py
+++ TorqueSpeedProfile.py
@@ -54,6 +54,7 @@ def run_b2b_dyno_test(config):
     ramp_settling_time_s = config['ramp_settling_time_s']
     max_rpm_ramp_step = config['max_rpm_ramp_step']
     max_current_ramp_step = config['max_current_ramp_step']
+    number_loops = int(config['number_loops'])
 
     last_rpm = 0
     last_current = 0
@@ -61,53 +62,57 @@ def run_b2b_dyno_test(config):
     log_file_name_ext = log_file_name + datetime.now().strftime("_%Y%m%d_%H%M%S") + '.txt'
 
     with open(log_file_name_ext, "w") as log_file:
-        with VESC(serial_port=motor_dut_port) as motor_dut:
-            with VESC(serial_port=motor_absorber_port) as motor_absorber:
+        with VESC(serial_port=motor_dut_port, timeout=0.1) as motor_dut:
+            with VESC(serial_port=motor_absorber_port, timeout=0.1) as motor_absorber:
 
                 log_file.write(f"Torque-Speed profile script\n")
                 log_file.write(f"DUT Firmware: {motor_dut.get_firmware_version()}\n")
                 log_file.write(f"Absorber Firmware: {motor_absorber.get_firmware_version()}\n")
                 log_file.write(f'time_ms, rpm, duty_cycle, v_in, avg_motor_current, avg_input_current, temp_fet, temp_motor\n')
 
-                for idx, rpm in rpm_array:
-                    current = current_array[idx]
-                    duration = duration_array[idx]
+                for i in range(number_loops):
+                    log_file.write(f"Iteration {i} out of {number_loops}\n")
+                    for idx, rpm in enumerate(rpm_array):
+                        current = current_array[idx]
+                        duration = duration_array[idx]
 
-                    do_rpm_ramp(motor_absorber, last_rpm, rpm, ramp_settling_time_s, max_rpm_ramp_step)
-                    do_current_ramp(motor_dut, last_current, current, ramp_settling_time_s, max_current_ramp_step)
+                        do_rpm_ramp(motor_absorber, last_rpm, rpm, ramp_settling_time_s, max_rpm_ramp_step)
+                        do_current_ramp(motor_dut, last_current, current, ramp_settling_time_s, max_current_ramp_step)
 
-                    nb_captures = int(duration / time_btwn_measurements)
-                    for _ in range(nb_captures):
-                        measurements_dut = motor_dut.get_measurements()
-                        if measurements_dut is None:
-                            log_file.write('Communication error with DUT - received None\n')
-                        else:
-                            log_measurements_to_file(measurements_dut, log_file)
-                            if measurements_dut.mc_fault_code != b'\x00':
-                                log_file.write(f'DUT faulted with code {measurements_dut.mc_fault_code}\n')
-                                isFaulted = True
-                                break
+                        nb_captures = int(duration / time_btwn_measurements)
+                        for _ in range(nb_captures):
+                            measurements_dut = motor_dut.get_measurements()
+                            if measurements_dut is None:
+                                log_file.write('Communication error with DUT - received None\n')
+                            else:
+                                log_measurements_to_file(measurements_dut, log_file)
+                                if measurements_dut.mc_fault_code != b'\x00':
+                                    log_file.write(f'DUT faulted with code {measurements_dut.mc_fault_code}\n')
+                                    isFaulted = True
+                                    break
 
-                        measurements_abs = motor_absorber.get_measurements()
-                        if measurements_abs is None:
-                            log_file.write('Communication error with Absorber - received None\n')
-                        else:
-                            if measurements_abs.mc_fault_code != b'\x00':
-                                log_file.write(f'Absorber faulted with code {measurements_abs.mc_fault_code}\n')
-                                isFaulted = True
-                                break
+                            measurements_abs = motor_absorber.get_measurements()
+                            if measurements_abs is None:
+                                log_file.write('Communication error with Absorber - received None\n')
+                            else:
+                                if measurements_abs.mc_fault_code != b'\x00':
+                                    log_file.write(f'Absorber faulted with code {measurements_abs.mc_fault_code}\n')
+                                    isFaulted = True
+                                    break
 
-                        time.sleep(time_btwn_measurements)
+                            time.sleep(time_btwn_measurements)
 
-                    last_rpm = rpm
-                    last_current = current
-                    do_current_ramp(motor_dut, last_current, 0, ramp_settling_time_s, last_current)
+                        last_rpm = rpm
+                        last_current = current
 
+                        if isFaulted:
+                            break
                     if isFaulted:
                         break
 
-                do_rpm_ramp(motor_absorber, last_rpm, 0, ramp_settling_time_s, max_rpm_ramp_step)
-                log_file.write('End of test sequence\n')
+                    do_current_ramp(motor_dut, last_current, 0, ramp_settling_time_s, last_current)
+                    do_rpm_ramp(motor_absorber, last_rpm, 0, ramp_settling_time_s, max_rpm_ramp_step)
+                    log_file.write(f'End of test sequence iteration {i}\n')
 
 
 if __name__ == "__main__":
